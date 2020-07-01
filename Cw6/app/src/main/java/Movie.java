ff1ffde992a56eef0e14f4a8a3d22a13e6f98d37
Nasser Khaled <nasserhuss123@gmail.com>
2020-07-01 18:57:24 +0300

    تم

---
+++ Cw6/app/src/main/java/Movie.java
@@ -0,0 +1,51 @@
+public class Movie {
+
+    private String title ;
+    private String mainActor ;
+    private Double movieRate ;
+    private int pgRate ;
+    private String genre ;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMainActor() {
+        return mainActor;
+    }
+
+    public void setMainActor(String mainActor) {
+        this.mainActor = mainActor;
+    }
+
+    public Double getMovieRate() {
+        return movieRate;
+    }
+
+    public void setMovieRate(Double movieRate) {
+        this.movieRate = movieRate;
+    }
+
+    public int getPgRate() {
+        return pgRate;
+    }
+
+    public void setPgRate(int pgRate) {
+        this.pgRate = pgRate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void bestmovie(Movie c1
+    , Movie c2);
+}
---
+++ Cw6/app/src/main/java/com/example/cw6/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.cw6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Movie;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Movie c1 = new Movie("Grown up","")
+    }
+}
